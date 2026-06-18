ff2688c41f7fb8e6328cceef679761da653a7e18
Satya Tulasi Jalandhar C H <satyatulasijalandharch2@gmail.com>
2026-06-18 10:59:42 +0530

    fix: improve the init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,14 +10,6 @@ fi
 
 rm -rf /workspaces/frappe_codespace/.git
 
-source /home/frappe/.nvm/nvm.sh
-nvm install 24
-nvm alias default 24
-nvm use 24
-
-npm install -g yarn
-
-echo "nvm use 24" >> ~/.bashrc
 cd /workspace
 
 bench init \
@@ -31,9 +23,9 @@ cd frappe-bench
 
 # Use containers instead of localhost
 bench set-mariadb-host mariadb
-bench set-redis-cache-host redis-cache:6379
-bench set-redis-queue-host redis-queue:6379
-bench set-redis-socketio-host redis-socketio:6379
+bench set-redis-cache-host redis://redis-cache:6379
+bench set-redis-queue-host redis://redis-queue:6379
+bench set-redis-socketio-host redis://redis-socketio:6379
 
 # Remove redis from Procfile
 sed -i '/redis/d' ./Procfile
